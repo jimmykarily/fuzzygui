f082d3408ad2f3184c7568527fab12389c6f4cea
Dimitris Karakasilis <jimmykarily@gmail.com>
2020-10-16 22:39:57 +0300

    Simplify code by removing go routines and channels

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.13
 require (
 	github.com/bep/debounce v1.2.0
 	github.com/gotk3/gotk3 v0.5.0
+	github.com/kylelemons/godebug v1.1.0 // indirect
 	github.com/sahilm/fuzzy v0.1.0
 )
--- main.go
+++ main.go
@@ -9,8 +9,17 @@ import (
 	"time"
 
 	"github.com/bep/debounce"
+	"github.com/gotk3/gotk3/gdk"
+	"github.com/gotk3/gotk3/glib"
 	"github.com/gotk3/gotk3/gtk"
 	"github.com/sahilm/fuzzy"
+)
+
+var (
+	lines        []string
+	listBox      *gtk.ListBox
+	dialogWindow *gtk.MessageDialog
+	currentMatch string
 )
 
 type employee struct {
@@ -54,79 +63,94 @@ func main() {
 		gtk.MainQuit()
 	})
 
-	dialogWindow := gtk.MessageDialogNew(win, gtk.DIALOG_MODAL|gtk.DIALOG_DESTROY_WITH_PARENT, gtk.MESSAGE_QUESTION, 0, "Fuzzy search")
+	dialogWindow = gtk.MessageDialogNew(win, gtk.DIALOG_MODAL|gtk.DIALOG_DESTROY_WITH_PARENT, gtk.MESSAGE_QUESTION, 0, "Fuzzy search")
 	dialogWindow.SetTitle("Fuzzy search")
 
 	dialogBox, _ := dialogWindow.GetContentArea()
 	userEntry, _ := gtk.EntryNew()
 	userEntry.SetSizeRequest(250, 0)
-	listBox, _ := gtk.ListBoxNew()
+	listBox, _ = gtk.ListBoxNew()
 	dialogBox.PackEnd(userEntry, false, false, 0)
 	dialogBox.PackEnd(listBox, false, false, 0)
 
-	patternChan := make(chan string)
-	matchesChan := make(chan []string)
-	go findMatches(patternChan, matchesChan)
-
 	debounced := debounce.New(100 * time.Millisecond)
-	userEntry.Connect("insert-text", func() {
+	userEntry.Connect("changed", func() {
 		debounced(func() {
-			s, _ := userEntry.GetText()
-			patternChan <- s
+			pattern, _ := userEntry.GetText()
+			matches := findMatches(pattern, &lines)
+			// Cleanup
+			_, err = glib.IdleAdd(CleanList)
+
+			numOfResults := 0
+			matchesLen := len(matches)
+			if matchesLen > 10 {
+				numOfResults = 10
+			} else {
+				numOfResults = matchesLen
+			}
+			for _, r := range matches[:numOfResults] {
+				label, _ := gtk.LabelNew(r)
+				_, err = glib.IdleAdd(listBox.Prepend, label)
+				_, err = glib.IdleAdd(listBox.ShowAll)
+			}
+			match := ""
+			if matchesLen > 0 {
+				match = matches[0]
+			}
+			_, _ = glib.IdleAdd(SelectFirstRow, match)
 		})
 	})
+
+	userEntry.Connect("key-press-event", func(entry *gtk.Entry, event *gdk.Event) {
+		if gdk.KeyvalFromName("Return") == gdk.EventKeyNewFromEvent(event).KeyVal() {
+			_, err = glib.IdleAdd(PrintSelectionAndExit)
+		}
+	})
+
+	go readLines()
 
 	dialogWindow.ShowAll()
 	dialogWindow.Run()
 	dialogWindow.GetDestroyWithParent()
-
-	for {
-		select {
-		case matches := <-matchesChan:
-			// Cleanup
-			listBox.GetChildren().Foreach(func(child interface{}) {
-				listBox.Remove(child.(gtk.IWidget))
-			})
-			for i, r := range matches {
-				label, _ := gtk.LabelNew(r)
-				listBox.Prepend(label)
-				if i == 0 {
-					listBox.SelectRow(listBox.GetRowAtIndex(i))
-				}
-			}
-		default:
-			time.Sleep(100 * time.Millisecond)
-		}
-	}
 }
 
-// Reads Stdin and receives the pattern on a channel
-// Prints matching results.
-func findMatches(patternChan chan string, matchesChan chan []string) {
-	reader := bufio.NewReader(os.Stdin)
-	lines := []string{}
-	results := []string{}
+func CleanList() {
+	listBox.GetChildren().Foreach(func(child interface{}) {
+		listBox.Remove(child.(gtk.IWidget))
+	})
+}
 
+func PrintSelectionAndExit() {
+	fmt.Print(currentMatch)
+	os.Exit(0)
+}
+
+func SelectFirstRow(match string) {
+	r := listBox.GetRowAtIndex(int(listBox.GetChildren().Length()) - 1)
+	listBox.SelectRow(r)
+	currentMatch = match
+}
+
+func readLines() {
+	reader := bufio.NewReader(os.Stdin)
 	for {
 		input, _, err := reader.ReadLine()
 		if err != nil && err == io.EOF {
 			break
 		}
+
 		lines = append(lines, string(input))
+		time.Sleep(time.Duration(1) * time.Microsecond)
+	}
+}
+
+// Looks for fuzzy matches in lines using the pattern
+func findMatches(pattern string, lines *[]string) []string {
+	matches := fuzzy.Find(pattern, *lines)
+	results := []string{}
+	for _, r := range matches {
+		results = append(results, (*lines)[r.Index])
 	}
 
-	for {
-		select {
-		case pattern := <-patternChan:
-			fmt.Println("new pattern:", pattern)
-			matches := fuzzy.Find(pattern, lines)
-			for _, r := range matches {
-				results = append(results, lines[r.Index])
-			}
-			fmt.Println("Will send: ", results)
-			matchesChan <- results
-		default:
-			time.Sleep(100 * time.Millisecond)
-		}
-	}
+	return results
 }
